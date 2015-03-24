5825242054df7f6048d833518aee1ee745dea38d
Amy Cotton <i7673459@bournemouth.ac.uk>
2015-03-24 13:38:52 +0000

    tried to add timestamp to the posts on index

--- includes/functions.php
+++ includes/functions.php
@@ -5,5 +5,33 @@
         exit; 
     }
 
+class convertToAgo {
+    
+    function convert_timedate($str) {
+        date_default_timezone_set('UTC')
+            list($date, $time) = explode(' ', $str);
+            list($year, $month, $day) = explode ('-', $date);
+            list($hour, $minute, $second) = explode (':', $time);
+            $timestamp = mktime($hour, $minute, $second, $month, $day, $year);
+            return $timestamp;
+        
+    }
+    
+    function makeAgo($timestamp) {
+        $difference = time() - $timestamp;
+        $periods = array("sec", "min", "hr", "day", "week", "month", "year", "decade");
+        $lengths = array("60","60","24","7","4.35","12","10");
+        for ($x = 0; $difference >=$lengths[$x]; $x++){
+            $difference /= $lengths[$x];
+            $difference = round($difference);
+            
+        }
+        
+    if ($difference != 1) $periods[$x].="s";
+        $text = "$difference $periods[$x] ago";
+        return $text;
+    }
+    
+}
 
 ?>
--- public/box1.php
+++ public/box1.php
@@ -1,7 +1,16 @@
-<div class="box1">              
+<?php
+    include_once("../includes/functions.php");
+    $timeAgoObject = new convertToAgo;
+    $ts = $row["dateofpost"];
+    $convertedTime = ($timeAGoObject -> convert_datetime($ts));
+?>
+
+<div class="box1">
         <h3><?php echo $row["full_name"]; ?></h3>
         <p class="teamtext"><?php echo $row["age"]; ?></p>
         <p class="teamtext"><?php echo $row["gender"]; ?></p>
         <p class="teamtext"><?php echo $row["bio"]; ?></p>
         <p class="teamtext"><?php echo $row["location"]; ?></p>
+    
+    
 </div>
