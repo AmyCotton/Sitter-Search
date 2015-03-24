2722c1ba9e97e56cd354d81258f1df5552fabe47
Amy Cotton <i7673459@bournemouth.ac.uk>
2015-03-24 14:15:52 +0000

    changed the background image

--- public/box1.php
+++ public/box1.php
@@ -1,11 +1,15 @@
-<?php
-    include_once("../includes/functions.php");
-    $timeAgoObject = new convertToAgo;
-    $ts = $row["dateofpost"];
-    $convertedTime = ($timeAGoObject -> convert_datetime($ts));
-?>
-
 <div class="box1">
+<?php
+    include_once("../includes/functions.php"); // Include the class library
+    $timeAgoObject = new convertToAgo; // Create an object for the time conversion functions
+    // Query your database here and get timestamp
+    $ts = $row["dateofpost"];
+    $convertedTime = ($timeAgoObject -> convert_datetime($ts)); // Convert Date Time
+    $when = ($timeAgoObject -> makeAgo($convertedTime)); // Then convert to ago time
+    ?>
+    
+    <div class="time_ago" <h2><?php echo "..".$when; ?></h2> </div>
+    
         <h3><?php echo $row["full_name"]; ?></h3>
         <p class="teamtext"><?php echo $row["age"]; ?></p>
         <p class="teamtext"><?php echo $row["gender"]; ?></p>
