18b4669fc51cff3245c33f054ee1a2ea4fb57bfb
Amy Cotton <i7673459@bournemouth.ac.uk>
2015-03-26 02:05:44 +0000

    sorting functions works and css editied

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,3 +1,9 @@
+<nav>
+  <ul class="pager">
+    <li><a href="#">Previous</a></li>
+    <li><a href="#">Next</a></li>
+  </ul>
+</nav>
         <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
         <script src="js/scripts.js"></script>
         <script src="js/bootstrap.js"></script>
--- public/index.php
+++ public/index.php
@@ -19,9 +19,39 @@
 ?>
 
 
+<?php
+
+	if(isset($_POST["sort-by"])) {
+
+		$sortBy = $_POST["sort-by"];
+
+		if(strcmp($sortBy, "age-desc") == 0) {
+			$query = "SELECT * FROM newpost ORDER BY age DESC";
+		} else if(strcmp($sortBy, "age-asc") == 0) {
+			$query = "SELECT * FROM newpost ORDER BY age ASC";
+		} else if(strcmp($sortBy, "male") == 0) {
+			$query = "SELECT * FROM newpost WHERE gender='male'";
+		} else if(strcmp($sortBy, "female") == 0) {
+			$query = "SELECT * FROM newpost WHERE gender='female'";
+		} else if(strcmp($sortBy, "recent") == 0) {
+			$query = "SELECT * FROM newpost ORDER BY user_id DESC";
+		}
+	} else if(isset($_POST["location-search"])) {
+		$searchQuery = $_POST["location"];
+		$query  = "SELECT * FROM newpost WHERE location LIKE '%{$searchQuery}%' ORDER BY user_id DESC";
+
+	} else {
+		$query = "SELECT * FROM newpost ORDER BY user_id DESC";
+	}
+
+
+
+
+
+?>
+
+
 <div class="container">
-
-
 
 <div class="carousel">
         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
@@ -35,18 +65,18 @@
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-      <img src="../public/images/plainbanner1.jpg">
+      <img src="../public/images/bluebanner.jpg">
       <div class="carousel-caption">
 
       </div>
     </div>
     <div class="item">
-      <img src="../public/images/plainbanner2.jpg"
+      <img src="../public/images/bluebanner2.jpg"
 
       </div>
     </div>
     <div class="item">
-      <img src="../public/images/plainbanner3.jpg">
+      <img src="../public/images/bluebanner3.jpg">
       <div class="carousel-caption">
 
       </div>
@@ -70,12 +100,41 @@
 
     <div class="row">
 
+        <div class=col-md-4>
+            <div class="search">
+                <div class="searchbox">
+                    <form action="index.php" method="post">
+                        <p><strong>Sort By:</strong></p>
+                        <select class="form-control" name="sort-by" onchange="this.form.submit()">
+                            <option>Please Select</option>
+                            <option value="age-desc">Age Desc</option>
+                            <option value="age-asc">Age Asc</option>
+                            <option value="male">Gender - Male</option>
+                            <option value="female">Gender - Female</option>
+                            <option value="recent">Most Recent</option>
+                        </select>
+                        <input class="hidden" type="submit" name="sort" value="Go">
+                    </form>
+
+                    <br>
+                    <br>
+
+                    <form action="index.php" method="post">
+                        <p><strong>Search by Location:</strong></p>
+                        <input class="form-control" type="text" name="location" value>
+                        <br>
+                        <input class="form-control" name="location-search" type="submit" value="Search">
+                    </form>
+
+                </div>
+            </div>
+        </div>
+
        <div class=col-md-8>
            <div class="row">
                <div class="col-md-12">
                    <div class="row post-row">
                         <?php
-                            $query = "SELECT * FROM newpost";
                             $result = mysqli_query($connection, $query);
 
                             if(!$result) {
@@ -93,33 +152,7 @@
             </div>
 		</div>
 
-        <div class=col-md-4>
-            <div class="search">
-                <div class="searchbox">
-                    <form action="index.php" method="get">
-                        <p><strong>Sort By:</strong></p>
-                        <select class="form-control" name="sort">
-                            <option>Please Select</option>
-                            <option value="age<"> Age < 30 </option?>
-                            <option value="age>"> Age > 30 </option?>
-                            <option value="gender">Gender</option?>
-                            <option value="recent">Most Recent</option?>
-                        </select>
-                    </form>
 
-                    <br>
-                    <br>
-
-                    <form action="index.php" method="get">
-                        <p><strong>Search by Location:</strong></p>
-                        <input class="form-control" type="text" name="location" value>
-                        <br>
-                        <input class="form-control" type="submit" value="Search"?
-                    </form>
-
-                </div>
-            </div>
-        </div>
     </div>
 
 </div>
